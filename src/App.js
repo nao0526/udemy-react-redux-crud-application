0c77234f219e4b9b25fd69378dd3393f37943d53
池田直人 <ike.climb7010@gmail.com>
2020-10-20 14:55:39 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return ( <div className="App"><h1>Hello World</h1></div> );
+class App extends Component{
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello World!!"
+//     );
+//   }
+// }
 
 export default App;
